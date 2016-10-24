88e0fd9903f236d9027adabb1e4f0f58f8e201a3
tosgit1989 <purannaa1989@gmail.com>
2016-10-24 15:37:38 +0900

    ArticlesController.php(コントローラーファイル)編集

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Models\Article;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+class ArticlesController extends Controller
+{
+
+    public function getIndex()
+    {
+        $articles = Article::all();
+
+        return view('articles.index')->with(compact('articles'));
+    }
+
+    public function getCreate()
+    {
+        return view('articles.create');
+    }
+
+    public function postCreate(Request $request)
+    {
+        $data = $request->all();
+        $article = new Article();
+        $article->title = $data['title'];
+        $article->comment = $data['title'];
+
+
+        $article->save();
+
+        return redirect()->to('/');
+    }
+
+    public function getEdit($id)
+    {
+        $article = Article::find($id);
+
+//        return view('edit')->withArticle($article);
+        return view('articles.edit', compact('article'));
+    }
+
+    public function postEdit(Request $request, $id)
+    {
+        $data = $request->all();
+        $article = Article::find($id);
+        $article->fill($data);
+
+        $article->save();
+
+        return redirect()->to('/');
+    }
+
+    public function getDelete($id)
+    {
+        $article = Article::find($id);
+
+//        return view('delete')->withArticle($article);
+        return view('articles.delete', compact('article'));
+    }
+
+    public function postDelete(Request $request, $id)
+    {
+        $data = $request->all();
+        $article = Article::find($id);
+        $article->fill($data);
+
+        $article->delete();
+
+        return redirect()->to('/');
+    }
+}
