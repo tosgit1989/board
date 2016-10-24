5def3bdca4ab9b41e8de2dec542345fb3274bf9c
tosgit1989 <purannaa1989@gmail.com>
2016-10-24 15:15:39 +0900

    app.php作成

---
+++ resources/views/app.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>board</title>
+    <link href="/css/board.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <p>board</p>
+        <div class="col-md-12">
+            @yield('content')
+        </div>
+    </div>
+</div>
+</body>
+</html>
