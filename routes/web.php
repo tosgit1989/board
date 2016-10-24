38c0485b7dd1800ddd13b129d67081f916629e73
tosgit1989 <purannaa1989@gmail.com>
2016-10-24 15:35:58 +0900

    web.php(ルーティングファイル)を編集

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,45 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+//Route::get('/', 'ArticlesController@getIndex');
+//Route::controller('articles', 'ArticlesController');
+
+Route::get('/', [
+    'uses' => 'ArticlesController@getIndex',
+    'as' => 'top',
+]);
+
+Route::get('/articles/new', [
+    'uses' => 'ArticlesController@getCreate',
+    'as' => 'create',
+]);
+
+Route::post('/articles/new', [
+    'uses' => 'ArticlesController@postCreate',
+    'as' => 'post_create',
+]);
+
+Route::get('/articles/edit/{id}', [
+    'uses' => 'ArticlesController@getEdit',
+    'as' => 'edit',
+]);
+
+Route::post('/articles/edit/{id}', [
+    'uses' => 'ArticlesController@postEdit',
+    'as' => 'post_edit',
+]);
+
+Route::get('/articles/delete/{id}', [
+    'uses' => 'ArticlesController@getDelete',
+    'as' => 'delete',
+]);
+
+Route::post('/articles/delete/{id}', [
+    'uses' => 'ArticlesController@postDelete',
+    'as' => 'post_delete',
+]);
