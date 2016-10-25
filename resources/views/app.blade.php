063f7e1641276ac5503b8d5ce95f687d1ceb5ccf
tosgit1989 <purannaa1989@gmail.com>
2016-10-25 16:23:33 +0900

    ヘッダーとフッターを追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,13 +6,21 @@
     <link href="/css/board.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <p>board</p>
-        <div class="col-md-12">
-            @yield('content')
+    <nav class="navbar navbar-fixed-top navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="active navbar-brand" href="/">board</a>
+            </div>
         </div>
+    </nav>
+    <div style="min-height: calc(100vh - 30px)">
+        <div style="height:50px; background-color:transparent"></div>
+        @yield('content')
     </div>
-</div>
+    <footer class="bs-docs-footer" style="background-color: #000000; height: 30px">
+        <div class="container">
+            <p class="text-muted"></p>
+        </div>
+    </footer>
 </body>
 </html>
